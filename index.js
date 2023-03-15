fbcc567184b407a3dff321bb656f2edeb1313ee6
liaojie1314 <1517438366@qq.com>
2023-03-15 18:46:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server Started on Port ${process.env.PORT}`);
+});
+
+app.get("/hello", (req, res) => {
+  res.json({
+    code: 200,
+    msg: "hello"
+  });
+});
