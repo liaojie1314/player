f01e7e0a6d188da1c5f915488acfa90154f11a64
unspamn <3064459131@qq.com>
2023-04-24 14:55:29 +0800

    ✨ feat: 添加点赞接口

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
-//解决跨域问题
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const apiRouter = require('./module/api');
-
 const app = express();
 require("dotenv").config();
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
-
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server Started on Port ${process.env.PORT}`);
 });
